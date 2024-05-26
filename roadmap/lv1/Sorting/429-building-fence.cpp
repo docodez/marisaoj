760b52ec694060c3720f3b6c511cc0b512780acd
theryou <phieu@duck.com>
2024-05-26 10:13:35 +0700

    add gitignore

---
+++ roadmap/lv1/Sorting/429-building-fence.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(NULL);    cin.tie(0);
+    int n;  cin >> n;
+    vector<pair<int, int> > a(n);
+    for (int i = 0; i < n; ++i) cin >> a[i].first;
+    for (int i = 0; i < n; ++i) cin >> a[i].second;
+    sort(a.begin(), a.end(), )
+    return 0;
+}
